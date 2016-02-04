df9b5e3bed51c808faf521e4be092d6850db150f
Madhav <madhav.khanna@tothenew.com>
2016-02-04 21:57:34 +0530

    T7 is completed

--- src/main/java/com/intelligrape/model/HibernateMain.java
+++ src/main/java/com/intelligrape/model/HibernateMain.java
@@ -9,24 +9,15 @@ import java.util.Date;
 public class HibernateMain {
 
     public static void main(String[] args) {
-        System.out.println("fdgdg");
-        UserDetails userDetails = new UserDetails("Madhav",new Date(),"First Address","desc");
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
+
+        User user = new User("Madhav");
         session.beginTransaction();
-        session.save(userDetails);
+        session.save(user);
+        session.save(new User("Priya"));
         session.getTransaction().commit();
         session.close();
-
-        userDetails = null;
-        session = sessionFactory.openSession();
-        session.beginTransaction();
-        // takes 2 arqument 1st class to be retrieved
-        // for fetch data we need to tell hibernate what model object we trying to fetch i.e class name
-        // like * from table name
-        // 2nd is primary key and find it value where id = 1
-        userDetails = (UserDetails)session.get(UserDetails.class,1);
-        System.out.println(userDetails.getUserName());
 
     }
 }
---
+++ src/main/java/com/intelligrape/model/User.java
@@ -0,0 +1,63 @@
+package com.intelligrape.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id // create PK (Surrogate key)
+//    @GeneratedValue
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue asking hibernate to generate value and it check the data type and generate value
+
+//        @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//It has some strategy and its of 4 type by default it auto
+    // auto means hibernate decide startegy to use to generate unique keys
+    private int userId;
+
+    private String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public User(){
+
+    }
+}
+
+
+
+
+/*
+*
+* Natural Key - one column has unique and it is required like email ID then it can be PK i.e key a Natural Key. Column which are there for business and assigned as PK is called NK
+*
+* surrogate key - more than one column can be marked as PK i.e a surrogate key
+*
+* In current system we do not know any column can marked as unique or we do not have it. In then that case we add a column to act as key
+* i.e called as surrogate key
+*
+* so,if it as NK then it is our duty to provide Value
+* and if it is surrogate key then we can ask hibernate to do that becoz it does not matter for us.
+*
+* */
