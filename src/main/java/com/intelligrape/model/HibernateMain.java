672b87ebf72c5ce7f166cbdc44d6f05af13ac895
Madhav <madhav.khanna@tothenew.com>
2016-02-05 18:30:42 +0530

    Code REfactor

--- src/main/java/com/intelligrape/model/HibernateMain.java
+++ src/main/java/com/intelligrape/model/HibernateMain.java
@@ -25,5 +25,43 @@ public class HibernateMain {
         session.getTransaction().commit();
         session.close();
 
+        session = sessionFactory.openSession();
+        user = null;
+        user =(User) session.get(User.class,1);
+
+        // this user object has list of address how to get the list w/o hibernate
+        //1. we get user;
+        //2. we get address were user_id is of above user;
+
+        // above command will give the user does the list also get pulled up
+        // the answer is yes
+
+        // let suppose we have many address . so, when we get user then all address will be pulled and increase cost
+
+        // now we can define fetch strategy
+
+        // by default Fetch is lazy and we can configure it
+
+        System.out.println(user.getUserName());
+        System.out.println("sdfsdf");
+        System.out.println(user.getAddressList());
+        System.out.println("sdfsdf 1");
+        System.out.println(user.getAddressList());
+
+        // how calling getter hibernate pull all values
+        /*
+        * hibernate uses proxy class to get data.Hibernate instead of getting actual we want it will get the proxy of that
+        * class that is a sub class of User class
+        *
+        * when we make a call to getListAdress() it has the code to get data and then it exceute the query get the data.
+        *
+        * after it does that then it do parent.getList()
+        *
+        *
+        * */
+
+
+
+
     }
 }
--- src/main/java/com/intelligrape/model/User.java
+++ src/main/java/com/intelligrape/model/User.java
@@ -12,66 +12,15 @@ import java.util.*;
 public class User {
 
     @Id // create PK (Surrogate key)
-
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @GeneratedValue(generator = "increment")
     private int userId;
 
     private String userName;
 
 
-    /*
-    * we do not have PK in user_address table and no index in it.
-    *
-    * How we can define it
-    *
-    * So,in order to define index we should use that data type which
-    * support index that is List not Set.
-    *
-    * But we choose Interface Collection for Some reason
-    *
-    * How to Define PK for Collection table
-    *
-    * @CollectionId
-    *
-    * all the other annotation are from jpa annotation
-    *
-    * that means all the annotation are jpa annotation
-    *
-    * hibernate jpa annotation implement and this feature is additional feature
-    *
-    *
-    * If we change our implementer(Hibernate) than this annotation will not work
-    *
-    * options in @CollectionId
-    *
-    * columns - to be defined as PK
-    * generator - how PK should be generated
-    * type - type of PK data type
-    *
-    * generator - we need to @GenericGenerator and define its name and strategy
-    * and its name is provide in generator
-    *
-    * Summary
-    *
-    * In order to have a collection of object as member variable inside the entity class
-    *
-    * 1. Change the data type of collection which supports index i.e list
-    * 2. @CollectionId annotation - tell this collection should have identifier
-    * 3. then define it property and for that we need generated and we need generic generator
-    *
-    * and we need to define id becoz address does not have id becoz it will persist in DB becoz it is an embedded object.
-    *
-    *
-    * */
 
-    @ElementCollection
-    @JoinTable(name = "user_address",joinColumns = @JoinColumn(name = "user_id")) // it will change the table name and join column name
-
-    @GenericGenerator(name = "hilo-gen",strategy = "hilo")
-//    @GenericGenerator(name = "hilo-gen",strategy = "uuid")
-//    @GenericGenerator(name = "hilo-gen",strategy = "increment")
-    @CollectionId(columns = {@Column(name = "address_id")},generator = "hilo-gen",type = @Type(type = "long"))
+    @ElementCollection()
+//    @ElementCollection(fetch = FetchType.EAGER)
      private Collection<Address> addressList = new ArrayList<Address>();
 
 
