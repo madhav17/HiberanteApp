b87eb3bf1da5bfd0ebf18c15dec53b665c4f212c
Madhav <madhav.khanna@tothenew.com>
2016-02-11 00:30:38 +0530

    T31 com

--- src/main/java/com/intelligrape/model/HibernateMain.java
+++ src/main/java/com/intelligrape/model/HibernateMain.java
@@ -5,6 +5,8 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.*;
@@ -24,31 +26,28 @@ public class HibernateMain {
 
         session.getTransaction().commit();
         session.close();
-
         session = sessionFactory.openSession();
         session.beginTransaction();
 
-        Criteria criteria = session.createCriteria(User.class);
-        criteria.add(Restrictions.eq("userName","Madhav10"))
-                .add(Restrictions.gt("userId",5));
+        // Projection is gerenally used for implementing agreegate function and grouping function
+        Criteria criteria = session.createCriteria(User.class)
+                            .setProjection(Projections.property("userId"))
+                            .addOrder(Order.desc("userId"));
 
-        criteria.add(Restrictions.like("userName","Madhav%"));
-        //or So,in that we need an OR clause
+        //aggregration
+//        Criteria criteria = session.createCriteria(User.class)
+//                .setProjection(Projections.max("userId"));
 
-        // Restrictions.or() takes 2 criteria
-        criteria.add(Restrictions.or(Restrictions.between("userId",1,5),Restrictions.eq("userName","Madhav10")));
-         List<User> userList =  criteria.list();
+    // ordering
+
+        List<Integer> userIdList = criteria.list();
+
+
         session.getTransaction().commit();
         session.close();
-        for(User user : userList){
-            System.out.println(user.getUserName());
+        for(Integer id : userIdList){
+            System.out.println(id);
         }
-
-
-//        Named query(HQL) is hibernate query language. HQL at certain point is not different from SQL
-//        as the query grows it is difficult to change and manage.
-
-//        therefore than comes criteria query
 
     }
 }
